8939362515695dd83a9c0f34528e9d7b1b828196
Bhuvan1100 <bhaskarbhuvan2004@gmail.com>
2025-02-16 19:57:45 +0000

    more on conversion

--- basics/03_conversion.js
+++ basics/03_conversion.js
@@ -7,15 +7,19 @@ let value2 = Number(score1);
 //console.log(value);
 //console.log(value2);
 
+
+// "33" can be conerted easily but "33abc" cannot
+// be converted to a number and result is nan
+
 let run = 28;
 let boolx = Boolean(run);
 //console.log(boolx);
 //console.log(typeof boolx);
 
-let str = "bhuvan";
-let str2 = ""
-let booly = Boolean(str);
-let boolz = Boolean(str2);
+// let str = "bhuvan";
+// let str2 = ""
+// let booly = Boolean(str);
+// let boolz = Boolean(str2);
 //console.log(boolz);
 //console.log(booly);
 
@@ -25,13 +29,38 @@ let num1 = Number(x);
 
 let y = undefined;
 let num2 = Number(y);
-console.log(num2);
-console.log(typeof num2);
-console.log(typeof y);
+// console.log(num2);
+// console.log(typeof num2);
+// console.log(typeof y);
+
+
+// let str1 = "hellow";
+// let str2 = " bhuvan";
+// let str3 = str1+str2;
+// console.log(str3);
+// console.log("1"+2+2);
+// console.log(1+2+2);
+// console.log(1+2+"2");
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+);// not executable
+
+let num = 100
+console.log(num++);
+num = 100
+console.log(++num);
 
 
 
 
-// "33" can be conerted easily but "33abc" cannot
-// be converted to a number and result is nan
+
+
+
+
+
+
+
+
 
