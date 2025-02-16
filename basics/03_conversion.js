c05c9f57ad2b17e542a61de1e3b5b18288c8b5f4
Bhuvan1100 <bhaskarbhuvan2004@gmail.com>
2025-02-16 19:10:58 +0000

    conversion of datatypes

---
+++ basics/03_conversion.js
@@ -0,0 +1,37 @@
+let score = "33";
+let score1 = "33abc"
+//console.log(typeof score);
+let value = Number(score);
+let value2 = Number(score1);
+//console.log(typeof(value));
+//console.log(value);
+//console.log(value2);
+
+let run = 28;
+let boolx = Boolean(run);
+//console.log(boolx);
+//console.log(typeof boolx);
+
+let str = "bhuvan";
+let str2 = ""
+let booly = Boolean(str);
+let boolz = Boolean(str2);
+//console.log(boolz);
+//console.log(booly);
+
+let x = null;
+let num1 = Number(x);
+//console.log(num1);
+
+let y = undefined;
+let num2 = Number(y);
+console.log(num2);
+console.log(typeof num2);
+console.log(typeof y);
+
+
+
+
+// "33" can be conerted easily but "33abc" cannot
+// be converted to a number and result is nan
+
