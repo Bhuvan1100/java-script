89cde3dbb27c408657557b55f832789aa3d10c84
Bhuvan1100 <bhaskarbhuvan2004@gmail.com>
2025-02-16 16:44:50 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // trest all JS code as newer version
+
+// alert(3+3) we are using node js and not browser
+
+console.log(3+3); 
+console.log("bhuvan");
+let username = "bhuvan"
+let age = 20
+let isLoggedIn = false
+// string 
+// bigint
+// boolean true and false
+// null standalone value
+// undefined
+// symbol = unique
+// object
+console.log(typeof "bhuvan");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+
+
+
+
