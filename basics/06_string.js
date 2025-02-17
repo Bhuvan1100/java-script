d58207b09558323bc5eb9b1de389d24977051147
Bhuvan1100 <bhaskarbhuvan2004@gmail.com>
2025-02-17 15:10:48 +0000

    more on string

---
+++ basics/06_string.js
@@ -0,0 +1,35 @@
+let user = "bhuvan"
+let id = "123"
+//console.log(username+id); not used
+
+//console.log(`hellow my name is ${username} ans my id is ${id}`);
+
+//let username = new String ("Bhuvan-Bhaskar-Deo");
+let username2 = "Bhuvan Bhaskar Deo";
+//console.log(username.length);
+//console.log(username.toUpperCase());
+
+//console.log(username.charAt(5));
+//console.log(username.chatAt(17)); // invalid since 17 char dosent exist
+
+// console.log(username.split('-'));
+// console.log(username.replace(/-/g,'*')); // replace all the - with * 
+// console.log(username.replace('-','*')) // eplace only the first occurance
+
+//console.log(username.trim());
+//console.log(username);
+// console.log(username2);
+// console.log(typeof username);
+// console.log(username2.length);
+// console.log(username[11]);
+// console.log(username2[2]);
+
+
+let username = new String ("Bhuvan-Bhaskar-Deo");
+// let username3 = username;
+// console.log(typeof username3);
+// username3 = new String ("BhuvanBhaskarDeo");
+// console.log(username);
+
+console.log(username.__proto__);
+username.__proto__
