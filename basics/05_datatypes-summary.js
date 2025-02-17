0495b7dc1e304b17b76b33a39aa0dc759d2b9bc5
Bhuvan1100 <bhaskarbhuvan2004@gmail.com>
2025-02-17 09:55:21 +0000

    more on datatypes

---
+++ basics/05_datatypes-summary.js
@@ -0,0 +1,52 @@
+/*
+primitive 
+7 types
+string, number, boolean, null, undefined, symbol, Bigint
+
+
+reference or non primitive
+arrays, objects, function
+*/
+
+
+// let username = undefined;
+// let username2 ; // both will initialize it as undefined
+// const id1 = Symbol('123');
+// const id2 = Symbol('123');
+// console.log(id1==id2);
+// console.log(id1===id2);
+
+
+
+// const bigNumber = 12347932039n; // add n to make it bigint
+// console.log(typeof bigNumber);
+// const bignumber2 = 1234575938493;
+// console.log(typeof bignumber2);
+
+
+
+
+// const heros = ["lion" , "tiger", "monkey"];
+// console.log(typeof heros); // all non primitives will give object as typeof 
+
+
+// let myObj = {
+//     user : "bhuvan",
+//     userid : 1234,
+// }
+// console.log(typeof myObj); 
+
+
+
+
+// const fun1 = function(){
+//     console.log("hellow world")
+// }
+// console.log(typeof fun1);
+
+
+// //symbol ka datatype symbol 
+
+
+
+
