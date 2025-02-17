d63d76a95e5c1aed6ae643dcd0e4abb2514d3139
Bhuvan1100 <bhaskarbhuvan2004@gmail.com>
2025-02-17 07:57:25 +0000

    datatype comparision

---
+++ basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2!=1);
+
+// console.log("2"==2);
+// console.log("2">1);
+
+// console.log(null == 0);
+// console.log(null>=0);
+// console.log(null >0);
+
+// console.log(undefined == 0);
+
+
+
+// === check datatype also 
+// console.log("2"==2); //true
+// console.log("2"===2) //false
+
+
+  // meaningless comparision
+console.log(undefined >= null);
+console.log(null < undefined);
+console.log(null=== undefined)
+console.log(undefined == null);
+
