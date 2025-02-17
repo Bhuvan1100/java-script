4860341eb5f187ebd0ef6f1e485a308dcd02c7fc
Bhuvan1100 <bhaskarbhuvan2004@gmail.com>
2025-02-17 23:08:49 +0530

    more on numbers

---
+++ basics/07_nums.js
@@ -0,0 +1,52 @@
+const score = 10;
+const num1 = new Number(200);
+// console.log(num);
+// console.log(score);
+
+//console.log(num1.toString().length);
+
+const num2 = 267.366;
+// console.log(num2.toFixed(1));
+// console.log(num2.toFixed(2));
+
+// console.log(Math.floor(num2))
+// console.log(Math.ceil(num2));
+// console.log(Math.round(num2));
+// console.log(Math.max(2,3,4,5)); // Math.min
+
+//console.log(Math.random());
+//console.log(Math.ceil(Math.random()*6) + 1);
+
+
+//console.log(ans);
+
+// for(let i=0; i<10; i++){
+//     const min = 1;
+//     const max = 6;
+//     const ans = Math.ceil(Math.random() * (max-min)) +1 ;
+//     console.log(ans);
+// }
+
+
+
+
+
+
+
+// const crypto = require('crypto');
+
+// // Generate a random number between 1 and 6
+// function rollDice() {
+//   const buffer = crypto.randomBytes(32); // 4 bytes (32 bits)
+//   const randomValue = buffer.readUInt32BE(0); // Convert to a 32-bit unsigned integer
+
+//   // Scale the random value to the range 1 to 6
+//   return (randomValue % 6) + 1;
+// }
+
+// console.log(rollDice());  // Outputs a random number between 1 and 6 
+
+// generating random numners 
+
+
+  
