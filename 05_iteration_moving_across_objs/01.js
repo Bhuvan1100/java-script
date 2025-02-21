01fcd84ab4e8d3c3cd99dbe66ed7ab706c402303
Bhuvan1100 <bhaskarbhuvan2004@gmail.com>
2025-02-21 13:55:37 +0000

    basics of arrays

---
+++ 05_iteration_moving_across_objs/01.js
@@ -0,0 +1,23 @@
+for (let index = 0; index <10; index++) {
+    const element = index;
+    console.log(element);   
+}
+// nothing much in this topic  only basics that you have studied in dsa topics and cp
+// only loops and nestes loops and some other topics 
+const arr = [1,2,3,4];
+console.log(arr.length)
+
+
+// just doing my coding so that i can be good at this and do codind at a better pace and work on documents at a greater pace
+// this lecture is quiet easy and has to be just seen at twice speed and require and no practise
+
+
+for (let index = 1; index <= 8; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue; // after if no other line is exexuted
+    }
+    console.log(`Value of i is ${index}`);
+   
+    
+}
