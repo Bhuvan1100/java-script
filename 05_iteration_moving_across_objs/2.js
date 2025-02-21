38115368fff9f64748f675f8aeb5015cd62aed6e
Bhuvan1100 <bhaskarbhuvan2004@gmail.com>
2025-02-21 19:27:03 +0530

    basics

---
+++ 05_iteration_moving_across_objs/2.js
@@ -0,0 +1,19 @@
+let index = 0;
+while(index<10){
+    console.log(index);
+    index++;
+}
+//same very easy lecture and nothing much and easy going and no new concept so just trying my hands on touch typing
+//do while
+var index1 = 1;
+do{
+  console.log(index1);
+  index1=index1+2;
+}while(index1<10);
+
+var score = 11;
+do{
+    console.log(score)
+    score++;
+}while(score<10); // after this break
+
